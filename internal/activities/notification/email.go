54447b3d0d8f9b0c953656a48d7f135d60310c47
Youmanvi <vihan.yerubandi@gmail.com>
2026-02-09 00:36:02 -0600

    fixed noificaion implemenaion

--- internal/activities/notification/email.go
+++ internal/activities/notification/email.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/vihan/taskorchestrator/internal/pkg/errors"
+	"github.com/Youmanvi/taskorchestrator/internal/pkg/errors"
 )
 
 // EmailNotificationInput is the input for sending email notifications
