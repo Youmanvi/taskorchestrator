1216a088316ee7f5c1ef8cc0732c4f4525a036ef
Youmanvi <vihan.yerubandi@gmail.com>
2026-02-10 00:30:39 -0600

    verif implemenaion

--- internal/activities/payment/verify.go
+++ internal/activities/payment/verify.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 
 	"github.com/shopspring/decimal"
-	"github.com/vihan/taskorchestrator/internal/pkg/errors"
+	"github.com/Youmanvi/taskorchestrator/internal/pkg/errors"
 )
 
 // VerifyPaymentInput is the input for verifying a payment
